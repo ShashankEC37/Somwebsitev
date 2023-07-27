2b2f94fbd41ab7f3edc8e7b4c0a6768882b17c6b
ShashankEC37 <jsr.shashank@gmail.com>
2023-07-27 09:44:55 +0530

    Shashank

--- src/components/UI/Testmonials.jsx
+++ src/components/UI/Testmonials.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../styles/testmonials.css"
 import Slider from "react-slick";
 
-import ava01 from '../../images/ava-1.jpg'
+
 import ava02 from '../../images/ava-2.jpg'
 import ava03 from '../../images/ava-3.jpg'
 
